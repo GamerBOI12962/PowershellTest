a919c5b53d0e2a0593221c2c44c03e6d67786200
GamerBOI12962 <113329382+GamerBOI12962@users.noreply.github.com>
2025-09-25 20:32:56 +0800

    Create main.ps1
    This is a test script

---
+++ main.ps1
@@ -0,0 +1,7 @@
+Write-Output "Hello, World!"
+$oranges=15
+$bananas=36
+$total=$oranges+$bananas
+Write-Output "There are $oranges oranges."
+Write-Output "There are $bananas bananas."
+Write-Output "There are $total in total."
