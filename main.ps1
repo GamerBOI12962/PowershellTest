3314052bbcf8e254ce12f3c6c0ae87fd75924d5e
GamerBOI12962 <113329382+GamerBOI12962@users.noreply.github.com>
2025-09-25 20:52:52 +0800

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -6,9 +6,10 @@ $score=0
 Write-Output "There are $oranges oranges."
 Write-Output "There are $bananas bananas."
 Write-Output "There are $total in total."
-function DetectOperatingSystem {if $IsWindows {$os=Windows} elseif $IsMacOs {$os=MacOS} elseif $IsLinux {$os=Linux}}
+function DetectOperatingSystem {if ($IsWindows) {$os=Windows} elseif ($IsMacOs) {$os=MacOS} elseif ($IsLinux) {$os=Linux}}
 function DisplayOperatingSystem {Write-Output "Your operating system is $os"}
 if (Get-Variable -Name IsWindows -ErrorAction SilentlyContinue) {$score=$score+1}
 if (Get-Variable -Name IsMacOs -ErrorAction SilentlyContinue) {$score=$score+1}
 if (Get-Variable -Name IsLinux -ErrorAction SilentlyContinue) {$score=$score+1}
+if ($score -ge 1) {DetectOperatingSystem}
 if ($score -ge 1) {DisplayOperatingSystem}
